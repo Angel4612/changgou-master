d987521dbf714b0803689f0ea4bdc30ce998ee4b
ez <oncemiss1@163.com>
2021-12-02 13:50:15 +0800

    品牌统计和规格统计

--- changgou_service/changgou_service_search/src/main/java/com/changgou/search/service/impl/SkuServiceImpl.java
+++ changgou_service/changgou_service_search/src/main/java/com/changgou/search/service/impl/SkuServiceImpl.java
@@ -8,6 +8,7 @@ import com.changgou.search.dao.SkuEsMapper;
 import com.changgou.search.pojo.SkuInfo;
 import com.changgou.search.service.SkuService;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.aggregations.Aggregation;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -61,7 +62,15 @@ public class SkuServiceImpl implements SkuService {
 
         // 设置分组条件, 商品分类
         nativeSearchQueryBuilder.addAggregation(
-                AggregationBuilders.terms("skuCategorygroup").field("categoryName").size(50));
+                AggregationBuilders.terms("skuCategoryGroup").field("categoryName").size(50));
+
+        // 设置分组条件, 商品品牌
+        nativeSearchQueryBuilder.addAggregation(
+                AggregationBuilders.terms("skuBrandGroup").field("brandName").size(50));
+
+        // 设置分组条件, 商品规格
+        nativeSearchQueryBuilder.addAggregation(
+                AggregationBuilders.terms("skuSpecGroup").field("spec.keyword").size(100));
 
         // 设置查询条件
         nativeSearchQueryBuilder.withQuery(QueryBuilders.matchQuery("name", keywords));
@@ -72,14 +81,20 @@ public class SkuServiceImpl implements SkuService {
         AggregatedPage<SkuInfo> skuPage = elasticsearchTemplate.queryForPage(query, SkuInfo.class);
 
         // 获取分组结果
-        StringTerms stringTerms = (StringTerms) skuPage.getAggregation("skuCategorygroup");
-        ArrayList<String> categoryList = new ArrayList<>();
-        if (Objects.nonNull(stringTerms)) {
-            for (StringTerms.Bucket bucket : stringTerms.getBuckets()) {
-                String keyAsString = bucket.getKeyAsString();
-                categoryList.add(keyAsString);
-            }
-        }
+        StringTerms stringTermsCategory = (StringTerms) skuPage.getAggregation("skuCategoryGroup");
+
+        List<String> categoryList = getStringList(stringTermsCategory);
+
+        // 获取分组结果 品牌
+        StringTerms stringTermsBrand = (StringTerms) skuPage.getAggregation("skuBrandGroup");
+
+        List<String> brandList = getStringList(stringTermsBrand);
+
+        // 获取分组结果 商品规格数据
+        StringTerms stringTermsSpec = (StringTerms) skuPage.getAggregation("skuSpecGroup");
+
+        Map<String, Set<String>> SpecList = getStringSetMap(stringTermsSpec);
+
 
         // 返回结果
         HashMap<String, Object> resultMap = new HashMap<>();
@@ -87,6 +102,59 @@ public class SkuServiceImpl implements SkuService {
         resultMap.put("total", skuPage.getTotalElements());
         resultMap.put("totalPages", skuPage.getTotalPages());
         resultMap.put("categoryList", categoryList);
+        resultMap.put("brandList", brandList);
+        resultMap.put("SpecList", SpecList);
         return resultMap;
     }
+
+    private Map<String, Set<String>> getStringSetMap(StringTerms stringTermsSpec) {
+        // 创建Map集合
+        HashMap<String, Set<String>> specMap = new HashMap<>();
+        // 创建Set集合
+        HashSet<String> specList = new HashSet<>();
+        // 先将信息存入set集合中, 存入的都是这样的字符串:
+        if (stringTermsSpec != null) {
+            for (StringTerms.Bucket bucket : stringTermsSpec.getBuckets()) {
+                specList.add(bucket.getKeyAsString());
+            }
+        }
+
+        // 遍历specList, 准备将里面的数据封装成Map, 然后返回
+        for (String specJson : specList) {
+            // 将specJson字符串, 转换成Map集合
+            Map<String, String> map = JSON.parseObject(specJson, Map.class);
+            // 遍历map集合
+            for (Map.Entry<String, String> entry : map.entrySet()) {
+                String key = entry.getKey(); // 规格名字
+                String value = entry.getValue(); // 规格选项
+                // 获取当前规格名字对应的规格数据
+                Set<String> specValues = specMap.get(key);
+                if (specValues == null) {
+                    specValues = new HashSet<String>();
+                }
+                // 将当前规格加入到集合中
+                specValues.add(value);
+                // 将数据存储到specMap中
+                specMap.put(key, specValues);
+            }
+        }
+        //
+        return specMap;
+    }
+
+    /**
+     * 根据StringTerms 获取集合
+     * @param stringTerms
+     * @return
+     */
+    private List<String> getStringList(StringTerms stringTerms) {
+        ArrayList<String> list = new ArrayList<>();
+        if (stringTerms != null) {
+            for (StringTerms.Bucket bucket : stringTerms.getBuckets()) {
+                list.add(bucket.getKeyAsString());
+            }
+        }
+        return list;
+    }
+
 }
